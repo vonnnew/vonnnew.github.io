d0d0e87a20573ef9df95c5e451e77da8c4a5fda0
Vonn New <vonn.new@gmail.com>
2025-10-02 16:28:19 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<meta charset="utf-8" />
+		<link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
+		<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">  
+		<title>Brilliant Ideals - The Omega Institute</title>
+<!--        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>  -->
+		<link href="../css/style.css?<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+
+</head>
+<body id="Ideals" lang="en-US">
+  <div id="header-container" class="full-width group">
+	<div id="site-logo" class="content-wrapper">
+	  <h3>OMEGA</h3>
+	</div>
+	<div id="header-nav">
+	    <h3><a href="#contact-us">Contact Us</a></h3>
+	</div>
+  </div> <!--header-container-->
+  <div id="hero-top" class="hero-b full-width group">
+	  <div class="content-wrapper over-image">
+        <img src="../image/logo_full.png">
+	  </div>
+ </div> <!--hero-->
+ <div id="blurb" class="content-wrapper group">
+     <p class="headline">AWAKENING THE BEST IN <br />THE 
+     HUMAN SPIRIT</p>
+	<p>Omega Institute was founded on a profound ideal &mdash; to help awaken the best in the human spirit. Over our 40-year history, we have consistently been at the forefront of driving social and cultural change, and have become one of the largest and most trusted centers for lifelong learning in the world.</p>
+ </div> <!--blurb-->
+<div id="gold1" class="content-wrapper group green1-background">
+    <center>
+    <img class="ideals1-img" src="../image/ideals_logo_NOcampaign_trick2.png">
+    </center>
+     <p><b>Brilliant Ideals: The Campaign for Omega</b> is a $30 million initiative that celebrates our growth and accomplishments, and boldly envisions what we can achieve in the future.</p>
+ </div> <!--gold1-->
+ 
+<div id="video-blurb" class="content-wrapper group">
+    <div style="padding-bottom: 70px;">
+     <p class="headline">INDIVIDUAL INSIGHT.<br /> COLLECTIVE WISDOM.<br /> GLOBAL IMPACT.</p>
+	</div>
+    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/258800049?color=2AD0CE&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+    <p class="view-video">Click above to view the video</p>
+
+
+</div> <!--video-blurb-->
+
+<div id="ideals1" class="content-wrapper group gold-background">
+    <center>
+    <img class="ideals1-img" src="../image/ideals_logo_NOcampaign_trick2.png">
+    </center>
+    <p>Through our leadership in holistic studies we have helped over one million people grow, learn, and find a deeper sense of purpose in their lives. </p>
+    <p>Our <i>Brilliant Ideals</i> campaign will enable us to become an even stronger force for change:</p>
+       <p style="padding: 40px 0!important; width: 83%; ">&bull;&nbsp;&nbsp;Enriching our Curriculum<br />
+        &bull;&nbsp;&nbsp;Energizing our Campus<br />
+        &bull;&nbsp;&nbsp;Engaging a Global Community</p>
+</div>
+
+<div id="ideals2" class="content-wrapper group">
+    <button class="accordion"><img src="../image/CURR.png"></button>
+      <div class="panel">
+        <p class="expanded">Omega is a hub for lifelong learning &mdash; offering workshops, professional trainings, and a wide variety of learning opportunities in-person and online. Through the <i>Brilliant Ideals</i> campaign we will:</p>
+        <p class="expanded"><b>Dramatically increase access to our curriculum and content</b> across multiple platforms to a growing global audience by investing in a digital campus that integrates campus-based education with online learning.</p>
+        <p class="expanded"><b>Develop vanguard education programming</b> by investing in R & D to keep pace with the quickly emerging educational needs of a rapidly changing global community.</p>
+        <p class="expanded"><b>Expand our Women's Leadership Center's programs</b> that inspire and train women to develop and use their leadership to #DoPowerDifferently.</p>
+        <p class="expanded"><b>Scale the catalytic initiative of our award winning Center for Sustainable Living</b> that demonstrates and teaches what is possible through regenerative thinking and design.
+          </div>
+    <button class="accordion"><img src="../image/CAMP.png"></button>
+      <div class="panel">
+        <p class="expanded">There has always been something unique and magical about the Omega campus.  Located in the heart of New York's Hudson Valley &mdash; a region long associated with art, natural wonder, and cultural innovation &mdash; Omega's living classroom offers an unparalleled opportunity for immersive learning.</p>
+        <p class="expanded">The <i>Brilliant Ideals</i> campaign will strengthen our leadership in sustainability and energize our Rhinebeck campus through strategic and much-needed improvements to our facilities.  Through these investments we will:</p>
+        <p class="expanded"><b>Renovate the Dining Hall,</b> preserving its historic character while modernizing its functionality as a model for cultivating a sustainable relationship to food.</p>
+        <p class="expanded"><b>Renovate the Wellness Center</b> to create new programming and educational opportunities and expand wellness services.</p>
+        <p class="expanded"><b>Create a new Arts Building</b> to enhance Arts programming at Omega.</p>
+        <p class="expanded"><b>Enhance the campus as a learning lab</b> with increased opportunities for community, contemplation, and exhibits that help educate outside the classroom.</p>
+        <p class="expanded"><b>Center regenerative design principles</b> in our Rhinebeck campus landscape</p>
+            </div>
+    <button class="accordion"><img src="../image/COMM.png"></button>
+      <div class="panel">
+        <p class="expanded">Through robust investments in our teaching and membership models and enhancements to our digital infrastructure and online platform, we can meet the needs of global learners by making our programs readily accessible and more affordable.  Additionally, this infrastructure will allow us to increase programming and advocacy that inspires our growing constituency to take action in their own lives and broader communities. The <i>Brilliant Ideals</i> campaign will enable us to:</p>
+        <p class="expanded"><b>Scale our global membership community</b> with strategic investments in our technological infrastructure, digital communications, and online programs.</p>
+        <p class="expanded"><b>Increase support for Omega's community engagement grants</b> to provide local nonprofits working retreats where they can connect, reflect, and strategize a brighter future.</p>
+        <p class="expanded"><b>Expand the Omega Scholarship Program</b> and the underwriting of key programs to provide greater affordability and access to our programs and enrich the Omega learning community.</p>
+        <p class="expanded"><b>Leverage Omega's assets and voice</b> to advance the growing environmental, social, cultural, and economic movements for change.</p>
+            </div>
+</div>
+
+
+<div id="modeling"class="content-wrapper group">
+  <center><p class="headline">OMEGA BY THE <br />NUMBERS</p></center>
+    <img src="../image/NEW_NUMBERS2.png">
+    <div id="impact">
+      <p class='headline'>OMEGA BY THE <br />IMPACT</p>
+    </div>
+    <img src="../image/Campaign_GRID.png">
+    <p class="headline">ACHIEVING OUR <br /><i>BRILLIANT IDEALS</i> </p>
+    <p>It will take the dedication, passion, and significant financial resources of many to achieve our ambitious vision for the future.  As Omega embarks on making its boldest contributions yet, we invite you to join us in our first-ever comprehensive campaign.</p>
+
+ 
+
+ </div> <!--modeling--> 
+
+ 
+   <div id="hero-bottom" class="hero-b full-width group">
+	  <div class="content-wrapper2 over-image">
+        <p class="headline">CAUSE & REFLECT</p>
+        <p>Envision a more just, peaceful, and sustainable future.
+        Join us in supporting <i>Brilliant Ideals: the Campaign for Omega</i>.  </p>
+	  </div>
+ </div> <!--hero-->
+
+<div id="contact">
+  <p class="headline">LEARN MORE</p>  
+  <center><p><a id="contact-us" href="mailto:development@eomega.org?Subject=Brilliant%20Ideals" target="_top">Development@eOmega.org</a> <br />or call <b>845-266-4444</b> x402</p></center>
+  <hr>
+</div>
+
+
+ <div id="footer">
+   <p class="foot-head"><b>&copy;</b> 2019 <a href="https://eomega.org">Omega Institute</a></p>
+   <p>150 Lake Drive,  
+    Rhinebeck, NY 12572 
+ </div>
+ 
+ 
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+</script>
+
+</body>
+</html>
